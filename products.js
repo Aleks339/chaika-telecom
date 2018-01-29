a7e1983810ad947ffb56dc189e42d7c9c35a9752
Aleks339 <annamuljar9@gmail.com>
2018-01-29 14:47:30 +0300

    json data continuing to add

--- products.js
+++ products.js
@@ -1,12 +1,12 @@
 $(function(){
     $.getJSON('products.json', function(data) {
-            for(var i=0; i<data.length; i++){
-                $('title').append(data.title[i]);
-                $('.product_code').append(data.code[i]);
-                $('img').append(data.primaryImageUrl[i]);
-                $('.product__link').append(data.description[i]);
-                $('.url--link').append(data.assocProducts[i]);
-                $('.url--link').append(data.assocProducts[i]);
+            for(var i=0; i<data.products.length; i++){
+                $('title').append(data.products.title[i]);
+                $('.product_code').append(data.products.code[i]);
+                $('img').append(data.products.primaryImageUrl[i]);
+                $('.product__link').append(data.products.description[i]);
+                $('.url--link').append(data.products.assocProducts[i]);
+                $('.url--link').append(data.products.assocProducts[i]);
             }
     });
 });
